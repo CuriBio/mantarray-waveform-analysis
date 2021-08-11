f6adfb92ccd5eb119980c282c5a4d60150b0e972
Kristian Eschenburg <keschenb@uw.edu>
2021-08-11 09:51:08 -0700

    add fraction max uuid to init file

--- src/mantarray_waveform_analysis/__init__.py
+++ src/mantarray_waveform_analysis/__init__.py
@@ -12,6 +12,7 @@ from .constants import BESSEL_LOWPASS_30_UUID
 from .constants import BUTTERWORTH_LOWPASS_30_UUID
 from .constants import CENTIMILLISECONDS_PER_SECOND
 from .constants import CONTRACTION_VELOCITY_UUID
+from .constants import FRACTION_MAX_UUID
 from .constants import IRREGULARITY_INTERVAL_UUID
 from .constants import MIDSCALE_CODE
 from .constants import MILLI_TO_BASE_CONVERSION
@@ -70,6 +71,7 @@ __all__ = [
     "SUBSEQUENT_VALLEY_INDEX_UUID",
     "AMPLITUDE_UUID",
     "AUC_UUID",
+    "FRACTION_MAX_UUID",
     "WIDTH_UUID",
     "WIDTH_VALUE_UUID",
     "WIDTH_RISING_COORDS_UUID",
